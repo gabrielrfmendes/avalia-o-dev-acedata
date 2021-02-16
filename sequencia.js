afe1e75f1b2064d1cac11c9a99bf9ed46f13a40f
Gabriel Mendes <gabrielrfmendes@gmail.com>
2021-02-16 11:11:16 -0300

    Alteração que impede o usuário de colocar um número repetido na sequência

--- sequencia.js
+++ sequencia.js
@@ -3,15 +3,20 @@ var sequencia = [];
 var qtdValores = prompt("Quantidade de valores: ");
 
 do {
-    sequencia.push(parseInt(prompt(`Valor: `)));
-    qtdValores--
+    var digito = parseInt(prompt(`Valor: `));
+    if (sequencia.indexOf(digito) != -1) {
+        alert('O número digitado já está na sequência.');
+    } else {
+        sequencia.push(digito);
+        qtdValores--
+    }    
 } while(qtdValores > 0);
 
 var maiorNumero = sequencia[0];
+var segundoMaiorNumero = sequencia[0];
 var menorNumero = sequencia[0];
 
 sequencia.forEach(element => {
-    console.log(element)
     if (element > maiorNumero) {
         maiorNumero = element
     }
@@ -19,9 +24,16 @@ sequencia.forEach(element => {
         menorNumero = element
     }
 });
+// 1 2 3 4 5 
+sequencia.forEach(element => {
+    if (element < maiorNumero && element > segundoMaiorNumero) {
+        segundoMaiorNumero = element        
+    }
+})
 
 document.write(`
     A sequência digitada foi ${sequencia}<br />
     O maior número na sequência é ${maiorNumero}<br />
     O menor número na sequência é ${menorNumero}<br />
+    O segundo maior número na sequência é ${segundoMaiorNumero}
 `)
